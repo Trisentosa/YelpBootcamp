cdb02a820573eec6584fc9a0c87d48e6cb31c398
Trisentosa W <trisentosawisesa@gmail.com>
2021-01-08 21:50:48 -0600

    add start script

--- app.js
+++ app.js
@@ -181,6 +181,6 @@ app.use((err, req, res, next) => {
 })
 
 const port = process.env.PORT || 3000
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log(`Connected to port ${port}`)
 })
